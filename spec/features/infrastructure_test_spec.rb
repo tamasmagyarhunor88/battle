71cea7022cd9c30ac4cc675c9d037c3f9ab86cc4
Magyar-Hunor Tamas <tamasmagyarhunor88@gmail.com>
2018-02-08 10:48:52 +0000

    add first test on page / add color format documentation to rspec

---
+++ spec/features/infrastructure_test_spec.rb
@@ -0,0 +1,7 @@
+
+feature 'Testing infrastructure working!' do
+  scenario 'checks if website displays information' do
+    visit('/')
+    expect(page).to have_content('Testing infrastructure working!')
+  end
+end

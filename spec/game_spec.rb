cf2d71804e20882890b6ff428f98ff0e879f8767
Magyar-Hunor Tamas <tamasmagyarhunor88@gmail.com>
2018-02-09 11:03:41 +0000

    Challenge skinny controllers DONE, get rid of many global(dangerous) variables

--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -1,12 +1,20 @@
 require 'game'
 
 describe Game do
-  subject(:game) { described_class.new }
+  subject(:game) { described_class.new(hannah, hunor) }
+  let(:hannah) { double('hannah') }
   let(:hunor) { double('hunor') }
-  let(:justyna) { double('justyna') }
+
+  it "retrieves the first player" do
+    expect(game.player1).to eq hannah
+  end
+
+  it "retrieves the second player" do
+    expect(game.player2).to eq hunor
+  end
 
   it "attacks second player" do
-    expect(justyna).to receive(:receive_damage)
-    game.attack(justyna)
+    expect(hunor).to receive(:receive_damage)
+    game.attack(hunor)
   end
 end
